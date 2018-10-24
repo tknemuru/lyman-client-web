24e082576404307c1d38c5ed69aa4cc4a9aa82dd
tknemuru <tknemuru@tknemuru.ne.jp>
2018-10-24 21:10:35 +0900

    to vue-cl

---
+++ lyman/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <span>{{context}}</span>
+    <span>{{hands}}</span>
+    <room-manage-dialog></room-manage-dialog>
+    <div class="tiles-container">
+      <ul v-for="(tile, i) in hands">
+        <li>
+          <img v-bind:src="buildTileImagePath(tile)">
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import RoomMangaeDialog from './assets/js/components/room-manage-dialog.vue'
+Vue.component(RoomMangaeDialog.name, RoomMangaeDialog);
+export default {
+  name: 'app',
+  data() {
+    return {
+      context: {},
+      hands: [],
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:25486/api/')
+    .then(response => {
+      this.context = response.data.context;
+      this.hands = this.context.hands[0];
+    })
+  },
+  methods: {
+    buildTileImagePath: function(tile) {
+      return require(`./assets/image/tiles/${tile}.png`);
+    }
+  },
+}
+</script>
+
+<style>
+ul {
+  list-style: none;
+  display: inline-block;
+  padding: 0px;
+}
+
+.tiles-container {
+  white-space: nowrap;
+}
+</style>
---
+++ lyman/src/assets/js/components/room-manage-dialog.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="modal-back">
+	<div class="modal-container">
+		<div>部屋を探す</div>
+		<div>部屋をつくる</div>
+	</div>
+</div>
+</template>
+<script>
+export default {
+	name: 'room-manage-dialog',
+}
+</script>
