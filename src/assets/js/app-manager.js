6d5acd53d53c9d93fec23870db10e393484073c2
tknemuru <tknemuru@tknemuru.ne.jp>
2018-10-24 11:59:52 +0900

    tile image disp

--- src/assets/js/app-manager.js
+++ src/assets/js/app-manager.js
@@ -1,6 +1,20 @@
 const app = new Vue({
 	el: '#app',
 	data: {
-		testVal: 'Hello World!'
-	}
+		testVal: 'Hello World!',
+		context: {},
+		hands: [],
+	},
+	mounted() {
+		axios.get('http://localhost:25486/api/')
+		.then(response => {
+			this.context = response.data.context;
+			this.hands = this.context.hands[0];
+		})
+	},
+	methods: {
+		buildTileImagePath: function(tile) {
+			return `/assets/image/tiles/${tile}.png`;
+		}
+	},
 });
