6044b05fe0746bb9e4188c63f34dde975f2f7d75
tknemuru <tknemuru@tknemuru.ne.jp>
2018-10-27 01:16:43 +0900

    room add

--- lyman/src/assets/js/components/room-manage-dialog.vue
+++ lyman/src/assets/js/components/room-manage-dialog.vue
@@ -1,12 +1,23 @@
 <template>
 <modal name="room-manage-dialog">
-	<div v-if="'top' === target" class="button-container">
-		<el-button type="primary" class="select-button" v-on:click="setShowTarget('search')">部屋を探す</el-button>
-		<el-button type="primary" class="select-button" v-on:click="setShowTarget('create')">部屋をつくる</el-button>
+	<div v-if="'registerName' === target" class="contents-container">
+		<div class="primary-text">名前を入力してください。</div>
+		<el-input placeholder="Please input" v-model="playerName" class="text-input"></el-input>
+		<el-button type="primary" v-on:click="setShowTarget('enterRoom')">決定</el-button>
 	</div>
-	<div v-if="'create' === target" class="create-container">
-		<span class="primary-text">部屋名を入力してください。</span>
-		<el-input placeholder="Please input" v-model="roomName"></el-input>
+	<div v-else-if="'enterRoom' === target" class="contents-container">
+		<el-button type="primary" v-on:click="setShowTarget('selectRoom')">部屋を探す</el-button>
+		<el-button type="primary" v-on:click="setShowTarget('createRoom')">部屋をつくる</el-button>
+	</div>
+	<div v-else-if="'createRoom' === target" class="contents-container">
+		<div class="primary-text">部屋名を入力してください。</div>
+		<el-input placeholder="Please input" v-model="roomName" class="text-input"></el-input>
+		<el-button type="primary" v-on:click="createRoom">決定</el-button>
+		<el-input placeholder="Please input" v-model="roomKey" class="text-input"></el-input>
+	</div>
+	<div v-else-if="'selectRoom' === target" class="contents-container">
+		<div class="primary-text">入室する部屋を選んでください。</div>
+		<el-input placeholder="Please input" v-model="roomName" class="text-input"></el-input>
 	</div>
 </modal>
 </template>
@@ -25,32 +36,40 @@ export default {
 		return {
 			target: undefined,
 			roomName: undefined,
+			playerName: undefined,
+			roomKey: undefined,
 		}
 	},
 	methods: {
 		show() {
-			this.setShowTarget();
+			this.setShowTarget('registerName');
 			this.$modal.show('room-manage-dialog');
 		},
 		hide() {
 			this.$modal.hide('room-manage-dialog');
 		},
 		setShowTarget(target) {
-			this.target = target || 'top';
+			this.target = target;
+		},
+		createRoom() {
+			axios.post('http://localhost:25486/api/createroom/', {
+				roomName: this.roomName,
+			})
+			.then(response => {
+				this.roomKey = response.data.roomKey;
+			})
 		},
 	},
 }
 </script>
 
 <style>
-.button-container {
-	text-align: center;
-}
-.select-button {
-	margin-top: 20%;
-}
-.create-container {
+.contents-container {
 	text-align: center;
 	margin-top: 20%;
+}
+.text-input {
+	width: 50%;
+	margin: 2%;
 }
 </style>
